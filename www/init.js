3965bfe87457210ec4f4a7261f4d112024fd53a7
John Roepke <john@justjohn.us>
2012-09-22 17:59:01 -0400

    Update manifest version to 2, use unpacked JS

---
+++ www/init.js
@@ -0,0 +1,14 @@
+
+// Prevent scrolling on Android / iOS devices
+document.ontouchmove = function(e){e.preventDefault();}
+document.ontouchstart = function(e){e.preventDefault();}
+
+// Kick everything off
+module.declare(["vendor/jquery", "app"], function() {
+    var $ = require("vendor/jquery").jQuery
+        , app = require("app");
+    app.analytics.register(window);
+    $(function() {
+        app.boot();
+    });
+});
