a82ef6badf0cdac79167841b4401e8207f11bfbf
John Roepke <john@justjohn.us>
2012-09-25 23:57:04 -0400

    Update node server to support 301 redirects and content-type for manifest files

--- server.js
+++ server.js
@@ -1,21 +1,78 @@
 #!/bin/env node
 
-var express = require("express");
+// General static file server for Node.js
+//
+// Supports
+//   - 301 Redirects
+//   - Content-Type headers for js, css, and manifest files
 
-// Mostly just a proxy for hosting the static files
+var REDIRECT_PROTOCOL = "http://",
+    REDIRECT_HOST = "www.flipclock.us";
 
-var app = express.createServer();
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.env.PORT || process.env.OPENSHIFT_INTERNAL_PORT || process.env.VCAP_APP_PORT || 8888,
+    host  = process.env.OPENSHIFT_INTERNAL_IP || "0.0.0.0";
 
-app.configure(function(){
-    app.use(express.static(__dirname + "/www"));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(express.bodyParser());
-});
+http.createServer(function(request, response) {
+  var hostname = request.headers.host;
 
-var host = process.env.HOST || "0.0.0.0",
-	port = process.env.PORT || 9999;
+  if (hostname !== REDIRECT_HOST)
+  {
+    var target = REDIRECT_PROTOCOL + REDIRECT_HOST + request.url;
+    console.log("Redirecting to " + target);
+    response.writeHead(301, {
+      'Location': target,
+      'Expires': (new Date).toGMTString()});
+    response.end();
+    return;
+  }
 
-app.listen(parseInt(port, 10), host);
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(__dirname, "www", uri);
 
-console.log("FlipClock is now running on " + host + ":" + port);
+  path.exists(filename, function(exists) {
+    if(!exists) {
+        filename = path.join(__dirname, "index.html");
+
+       response.writeHead(404, {"Content-Type": "text/plain"});
+       response.write("404 Not Found :( Sorry.\n");
+       response.end();
+       return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var opts;
+      if (filename.indexOf(".js") > 0) {
+          opts = {"Content-Type": "application/x-javascript"};
+
+      } else if (filename.indexOf(".css") > 0) {
+          opts = {"Content-Type": "text/css"};
+
+      } else if (filename.indexOf(".manifest") > 0) {
+          opts = {"Content-Type": "text/cache-manifest"};
+
+      } else if (filename.indexOf(".webapp") > 0) {
+          opts = {"Content-Type": "application/x-web-app-manifest+json"};
+      }
+
+      response.writeHead(200, opts);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10), host);
+
+console.log("FlipClock running at\n => http://" + host + ":" + parseInt(port, 10) + "/\nCTRL + C to shutdown");
 
