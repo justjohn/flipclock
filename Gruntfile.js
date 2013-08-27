5849a9cfb5d8cf5ec5f4da2f47fc4abaabdca9db
John Roepke <john@justjohn.us>
2013-08-26 22:38:23 -0400

    Add grunt task for building less files

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.initConfig({
+    	pkg: grunt.file.readJSON('package.json'),
+    	less: {
+    		development : {
+				options: {
+					report: 'min',
+					yuicompress: true,
+					paths: ["www/css"]
+				},
+				files: {
+					"www/css/app.css": "www/css/app.less"
+				}
+			}
+    	},
+    	watch: {
+			scripts: {
+				files: ['**/*.less'],
+				tasks: ['less'],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+
+	});
+
+}
