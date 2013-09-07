7714d1d4e5ac875cb5367e9a12705960d70037bf
John Roepke <john@justjohn.us>
2013-09-07 00:56:29 -0400

    Don't compress CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
     		development : {
 				options: {
 					report: 'min',
-					yuicompress: true,
+					yuicompress: false,
 					paths: ["www/css"]
 				},
 				files: {
